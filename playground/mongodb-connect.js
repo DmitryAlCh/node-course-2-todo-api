b90dddbcea78d26176e64e46186eb7729302c3c1
DmitryAlCh <dmi.al.ch@gmail.com>
2017-02-14 11:37:27 +0200

    objectID discussed

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,17 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+// var user = {name:"Dmitry", age:33};
+// var {name} = user;
+// console.log(name);
+
+// object destructuring, takes any property out of an object
+// var {needed property name} = objectName;
+
+// var obj = new ObjectID();
+// console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err){
@@ -18,15 +31,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 //always close connection
 
 // insert new doc into Users (name,age,location)
-  db.collection('Users').insertOne({
-    name: "Dmitry",
-    age: 33,
-    location: "Latvia"
-  },(err, success)=>{
-    if (err){
-      return console.log('Unable to add user', err);
-    }
-    console.log(JSON.stringify(success.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: "Dmitry",
+  //   age: 33,
+  //   location: "Latvia"
+  // },(err, success)=>{
+  //   if (err){
+  //     return console.log('Unable to add user', err);
+  //   }
+  //   console.log(JSON.stringify(success.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
   db.close();
 });
