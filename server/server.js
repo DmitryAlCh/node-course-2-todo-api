932c83c51b6fae9e9d9ae1ecf3a217ae1f385090
DmitryAlCh <dmi.al.ch@gmail.com>
2017-02-15 12:18:04 +0200

    added validators to schema

--- server/server.js
+++ server/server.js
@@ -6,13 +6,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true //removes all whitespaces befor and after text
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
@@ -27,13 +32,37 @@ var newTodo = new Todo({
 // });
 
 var secondTodo = new Todo({
-  text: 'Eat dinner',
-  completed: false,
-  completedAt: 11
+  text: '  Edit code '
 });
 
-secondTodo.save().then((doc)=>{
-  console.log('Saved new todo ', doc);
-},(e)=>{
-  console.log('Unable to save new todo ', e);
+// secondTodo.save().then((doc)=>{
+//   console.log('Saved new todo ', doc);
+// },(e)=>{
+//   console.log('Unable to save new todo ', e);
+// });
+
+// User
+// email - require it - trim it, set type, set min lenght.
+
+var User = mongoose.model('Users', {
+  email: {
+    required: true,
+    type: String,
+    trim: true,
+    minlength: 3,
+  }
 });
+
+var firstUser = new User({
+  email: 'dmitry@mail.com'
+});
+
+firstUser.save().then(
+  (succesMessage)=>{
+    console.log('User added');
+    console.log(JSON.stringify(succesMessage, undefined, 2));
+  },
+  (errorMessage)=>{
+    consoe.log('Unable to add user: ', errorMessage);
+  }
+);
