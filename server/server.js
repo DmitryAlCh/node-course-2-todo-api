b5b824c22ca1ee6852d1fde521c2dd23db98f8b4
DmitryAlCh <dmi.al.ch@gmail.com>
2017-02-16 11:35:02 +0200

    Add GET / Todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,14 @@ app.post('/todos', (req, res) => {
   );
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((success) =>{
+    res.send({todos:success});
+  }, (err) =>{
+    res.status(400).send(err);
+  });
+});
+
 
 app.listen(3000, ()=>{
   console.log('Starter on port 3000');
